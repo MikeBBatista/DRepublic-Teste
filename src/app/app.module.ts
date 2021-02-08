b5a453630da87c8eb41cdfb644c5370103ee6703
MikeBBatista <46934773+MikeBBatista@users.noreply.github.com>
2021-02-08 09:55:30 -0300

    Styles bootstrap (#7)
    * instalando bootstrap e ng-bootstrap
    
    * alinhando elementos do html
    
    * trocando cor do background e linguagem da pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaintCalculatorModule } from './paint-calculator/paint-calculator.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { PaintCalculatorModule } from './paint-calculator/paint-calculator.modul
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PaintCalculatorModule
+    PaintCalculatorModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
