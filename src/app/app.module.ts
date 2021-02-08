0bd3658523122aa9c5b480d8ed84c13a328b18ff
Mike Barcelos <mikebbatista@hotmail.com>
2021-02-07 21:30:55 -0300

    Criacao do componente calculador da quantidade de tinta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaintCalculatorComponent } from './paint-calculator/paint-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaintCalculatorComponent
   ],
   imports: [
     BrowserModule,
