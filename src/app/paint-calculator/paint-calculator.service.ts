edf65a1e7a265aa7a2ce47e40c871c9c0de77031
Mike Barcelos <mikebbatista@hotmail.com>
2021-02-08 00:16:02 -0300

    Calculando area da parede, das janelas e portas e verificando se o tamanho das paredes e valido

--- src/app/paint-calculator/paint-calculator.service.ts
+++ src/app/paint-calculator/paint-calculator.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { ElementRef, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +6,21 @@ import { Injectable } from '@angular/core';
 export class PaintCalculatorService {
 
   constructor() { }
+
+  wallArea(sideA: number, sideB: number){
+    return sideA * sideB;
+  }
+
+  winDoorArea(winCount: number, doorCount: number ) {
+    const totalArea = (winCount * 2.4) + (doorCount * 1.52);
+    return totalArea;
+  }
+
+  validWalls(wallSide : ElementRef){
+    const inputValue = Number(wallSide.nativeElement.value);
+    if (inputValue < 1 || inputValue > 15){
+      alert('Número inserido está fora do range permitido!');
+      wallSide.nativeElement.value = '';
+    }
+  }
 }
